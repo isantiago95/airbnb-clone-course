9948463c3300ec581208c59229ded206e2c87ebc
Israel Santiago <israel.santiago4359@gmail.com>
2025-02-03 18:57:02 -0800

    adding navbar components, routes, clerk, and auth routes

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import { SignOutButton } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
+
 function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast();
+
+  const handleLogout = () => {
+    toast({ description: "You have been logged out" });
+  };
+
+  return (
+    <SignOutButton redirectUrl="/">
+      <button className="w-full text-left" onClick={handleLogout}>
+        logout
+      </button>
+    </SignOutButton>
+  );
 }
 
 export default SignOutLink;
