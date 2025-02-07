e13bffffad92a2b94b83f17a604269826701d302
Israel Santiago <israel.santiago4359@gmail.com>
2025-02-06 19:02:05 -0800

    fetch properties and home page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
-import { Button } from "@/components/ui/button";
+import CategoriesList from "@/components/home/CategoriesList";
+import PropertiesContainer from "@/components/home/PropertiesContainer";
 
-const HomePage = () => {
+const HomePage = ({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) => {
   return (
-    <div>
-      <h1 className="text-3xl">HomeAway Project - Starter</h1>
-      <Button variant="outline" size="lg" className="capitalize m-8">
-        click me
-      </Button>
-    </div>
+    <section>
+      <CategoriesList
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+      <PropertiesContainer
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+    </section>
   );
 };
 export default HomePage;
